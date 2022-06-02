33c7c5403eb660f65253670cb433a2209c1849b6
Alex Kräuchi <alex@smallstack.com>
2022-06-02 14:46:24 +0200

    Solve DrumKit challenge

---
+++ 01 JavaScript Drum Kit/app.js
@@ -0,0 +1,19 @@
+const drumKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+
+document.addEventListener('keydown', event => {
+    if (drumKeys.includes(event.key)) {
+        const pressedKey = document.querySelector(`div[data-key="${event.key}"]`)
+        const audio = document.querySelector(`audio[data-key="${event.key}"]`)
+        if (!pressedKey || !audio) return;
+        pressedKey.classList.add('playing');
+        audio.play();
+    }
+})
+
+document.addEventListener('keyup', event => {
+    if (drumKeys.includes(event.key)) {
+        const releasedKey = document.querySelector(`div[data-key="${event.key}"]`)
+        if (!releasedKey) return;
+        releasedKey.classList.remove('playing');
+    }
+})
